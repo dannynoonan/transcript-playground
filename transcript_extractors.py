57918a2a5eb9c795ed9d42b004acaff413f2d932
Andy Shirey <andyshirey@ilovelamp.local>
2023-06-25 20:14:36 -0700

    GoT Fanon transcript extractor lookin pretty dialed in, let's see how re-usable both the generic and specific components are to other shows and transcript types

--- transcript_extractors.py
+++ transcript_extractors.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 
 from type_system import Episode, Scene, SceneEvent, SceneDialog
 
@@ -17,20 +18,25 @@ class TranscriptExtractor(object):
             transcript_raw = transcript_soup.find_all('div', class_='mw-parser-output')[0]
             transcript_lines = transcript_raw.find_all('p')
 
-            # episode_open_prefixes = ['[Episode opens ', '[Episodes opens ', 'TITLE SEQUENCE']
-            scene_change_prefixes = ['CUT TO: ', 'EXT. ']
+            scene_change_prefixes = ['CUT TO: ', 'EXT. ', 'INT. ']
 
             first_line = True
             for line in transcript_lines:
                 line = line.get_text()
-                line = line.rstrip()
 
+                # preliminary trimming
+                line = line.replace(").", ".)")
+                line = line.replace("].", ".]")
+                line = line.strip()
+
+                # first line may not have distinct prefix text but needs to initialize a scene
                 if first_line:
                     first_line = False
                     scene = Scene()
                     scene.description = line
                     episode.scenes.append(scene)
 
+                # if line starts with a scene_change_prefix -> initialize new scene
                 elif line.startswith(tuple(scene_change_prefixes)):
                     prefix_len = 0
                     for pref in scene_change_prefixes:
@@ -46,13 +52,17 @@ class TranscriptExtractor(object):
                         scene.location = line[prefix_len:]
                     episode.scenes.append(scene)
 
+                # otherwise line is either dialog or context_info within a scene
                 else:
                     line_bits = line.split(': ', 1)
+                    # if line contains a colon, we assume it splits a character name and their dialog
                     if len(line_bits) > 1:
                         dialog = SceneDialog()
                         dialog.spoken_by = line_bits[0]
-                        dialog.line = line_bits[1]
+                        # if line contains substrings wrapped in brackets or parens, extract these into context_info
+                        extract_context_info_from_dialog(line_bits[1], dialog)
                         scene.events.append(dialog)
+                    # otherwise assume the line is context_info between lines of dialog within a scene
                     else:
                         event = SceneEvent()
                         if line[0] == '[':
@@ -66,4 +76,24 @@ class TranscriptExtractor(object):
                     scene.description = scene.events[0].context_info
                     scene.events = scene.events[1:]
 
+        print('----------------------------------------------------------------------------')
+        print(f'len(episode.scenes)={len(episode.scenes)}')
+
         return episode
+    
+
+'''
+If line contains substrings wrapped in brackets or parens, extract these into context_info
+'''
+def extract_context_info_from_dialog(raw_line: str, dialog: SceneDialog) -> None:
+    ctx_info = re.findall(r'[\(\[].*?[\)\]]', raw_line)
+    if len(ctx_info) == 0:
+        dialog.line = raw_line
+    else:
+        dialog.line = re.sub("[\(\[].*?[\)\]]", "", raw_line)
+        dialog.context_info = ''
+        for ci in ctx_info:
+            dialog.context_info = f'{dialog.context_info}{ci[1:-1]} '
+
+        dialog.line = dialog.line.strip()
+        dialog.context_info = dialog.context_info.strip()
