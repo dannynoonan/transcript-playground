068a7e8a37b1b56fa8faea397a1b75fac81d996e
Andy Shirey <andyshirey@ilovelamp.lan>
2024-02-06 18:22:18 -0800

    restored correct cluster sequences to arrays fed to plotly scatter... now to figure out what went wrong

--- app/web/fig_builder.py
+++ app/web/fig_builder.py
@@ -21,17 +21,20 @@ def generate_graph_matplotlib(df: pd.DataFrame, show_key: str, num_clusters: int
     plt.rcParams['figure.autolayout'] = True
     fig = plt.figure()  # make sure to call this, in order to create a new figure
 
-    # draw figure
-    tsne = TSNE(n_components=2, perplexity=15, random_state=42, init="random", learning_rate=200)
-
     # vis_dims2 = tsne.fit_transform(matrix)
 
+    # fp = f'clusters_matplotlib_{show_key}_{num_clusters}.csv'
+    # df.to_csv(fp)
+
+    # draw figure
+    tsne = TSNE(n_components=2, perplexity=15, random_state=42, init="random", learning_rate=200)
     new_df = df.copy()
     new_df = new_df.drop(['doc_id', 'Cluster'], axis=1)
     new_df.columns = new_df.columns.astype(str)
+    print(f'*** generate_graph_matplotlib new_df.columns={new_df.columns}')
     vis_dims2 = tsne.fit_transform(new_df)
 
-    # print(f'len(vis_dims2)={len(vis_dims2)} vis_dims2={vis_dims2}')
+    print(f'matplotlib len(vis_dims2)={len(vis_dims2)} vis_dims2={vis_dims2}')
 
     x = [x for x, y in vis_dims2]
     y = [y for x, y in vis_dims2]
@@ -47,9 +50,9 @@ def generate_graph_matplotlib(df: pd.DataFrame, show_key: str, num_clusters: int
         ys = np.array(y)[df.Cluster == category]
         plt.scatter(xs, ys, color=color, alpha=0.3)
 
-        # print(f'category={category}, color={color}')
-        # print(f'xs={xs}')
-        # print(f'ys={ys}')
+        print(f'matplotlib category={category}, color={color}')
+        print(f'xs={xs}')
+        print(f'ys={ys}')
 
         avg_x = xs.mean()
         avg_y = ys.mean()
@@ -76,10 +79,14 @@ def generate_graph_plotly(doc_clusters_df: pd.DataFrame, merged_df: pd.DataFrame
     fig_height = fm.fig_dims.hdef(fig_width)
     base_fig_title = f'{num_clusters} clusters for {show_key} visualized in 2D using t-SNE'
 
+    # fp = f'clusters_plotly_{show_key}_{num_clusters}.csv'
+    # doc_clusters_df.to_csv(fp)
+
     tsne = TSNE(n_components=2, perplexity=15, random_state=42, init="random", learning_rate=200)
     new_df = doc_clusters_df.copy()
     new_df = new_df.drop(['doc_id', 'Cluster', 'cluster_color'], axis=1)
     new_df.columns = new_df.columns.astype(str)
+    print(f'*** generate_graph_plotly new_df.columns={new_df.columns}')
     vis_dims2 = tsne.fit_transform(new_df)
 
     # doc_clusters_df['cluster_color'] = doc_clusters_df['Cluster'].apply(lambda x: fm.colors[x])
@@ -103,22 +110,24 @@ def generate_graph_plotly(doc_clusters_df: pd.DataFrame, merged_df: pd.DataFrame
     # print(f'len(y)={len(y)} y={y}')
 
     # init figure with core properties
-    fig = px.scatter(merged_df, x=x, y=y, color=merged_df.cluster_color,
-                title=base_fig_title, custom_data=custom_data,
-                hover_name=merged_df.doc_id, hover_data=hover_data,
+    fig = px.scatter(doc_clusters_df, x=x, y=y, color=doc_clusters_df.cluster_color,
+                title=base_fig_title, 
+                # custom_data=custom_data,
+                hover_name=doc_clusters_df.doc_id, 
+                # hover_data=hover_data,
                 height=fig_height, width=fig_width, opacity=0.7)
     # axis metadata
     fig.update_xaxes(title_text='x axis of t-SNE')
     fig.update_yaxes(title_text='y axis of t-SNE')
 
-    fig.update_traces(
-        hovertemplate = "<br>".join([
-            "<b>%{customdata[0]}</b><br>",
-            "Season: <b>%{customdata[1]}</b>",
-            "Sequence: <b>%{customdata[2]}</b>",
-            "Air date: <b>%{customdata[3]}</b>",
-            "Episode key: <b>%{customdata[4]}</b>"
-        ])
-    )
+    # fig.update_traces(
+    #     hovertemplate = "<br>".join([
+    #         "<b>%{customdata[0]}</b><br>",
+    #         "Season: <b>%{customdata[1]}</b>",
+    #         "Sequence: <b>%{customdata[2]}</b>",
+    #         "Air date: <b>%{customdata[3]}</b>",
+    #         "Episode key: <b>%{customdata[4]}</b>"
+    #     ])
+    # )
 
     return fig
